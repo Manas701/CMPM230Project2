6153d59c5f641ef42c1e44a62d6cd69cd9a32cf2
Menace791 <manas.s.sara@gmail.com>
2023-10-12 16:23:56 -0700

    made most variables global

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
 
-void drawGrid(sf::RenderWindow &window, float lineWidth, float indentWidth, float gridLength, int gridSize)
+const float windowLength = 800.0f;
+const float indentPercent = 0.05;
+const float indentWidth = windowLength * indentPercent; // 40
+const float lineWidth = 1.0f;
+const float gridLength = windowLength - (2*indentWidth); // 720
+const int gridSize = 8; // 40 to 760
+const float squareLength = gridLength / gridSize; // 22.5
+const float paletteLength = 200;
+const float paletteHeight = 400;
+int tileMap[gridSize][gridSize] = {};
+std::map<int, sf::Texture> paletteMap;
+
+sf::RenderWindow window(sf::VideoMode(windowLength, windowLength), "Tilemap :)");
+sf::RenderWindow palette(sf::VideoMode(paletteLength, paletteHeight), "Palette :I");
+
+void drawGrid()
 {
     sf::RectangleShape line(sf::Vector2f(gridLength, lineWidth));
 
@@ -21,7 +36,7 @@ void drawGrid(sf::RenderWindow &window, float lineWidth, float indentWidth, floa
     }
 }
 
-std::tuple<int, int> getTilePosition(sf::RenderWindow &window, float windowLength, float indentWidth, float gridLength, int gridSize, float squareLength)
+std::tuple<int, int> getTilePosition()
 {
     float gridPosX = sf::Mouse::getPosition(window).x - indentWidth;
     float gridPosY = sf::Mouse::getPosition(window).y - indentWidth;
@@ -33,9 +48,9 @@ std::tuple<int, int> getTilePosition(sf::RenderWindow &window, float windowLengt
     return std::make_tuple(gridPosX / squareLength, gridPosY / squareLength);
 }
 
-void drawIndicator(sf::RenderWindow &window, float windowLength, float indentWidth, float gridLength, float gridSize, float squareLength, float lineWidth)
+void drawIndicator()
 {
-    std::tuple<int, int> tilePosition = getTilePosition(window, windowLength, indentWidth, gridLength, gridSize, squareLength);
+    std::tuple<int, int> tilePosition = getTilePosition();
     if (std::get<0>(tilePosition) >= 0)
     {
         sf::RectangleShape hoverIndicator(sf::Vector2f(squareLength-lineWidth, squareLength-lineWidth));
@@ -47,18 +62,8 @@ void drawIndicator(sf::RenderWindow &window, float windowLength, float indentWid
 
 int main()
 {
-
-    const float windowLength = 800.0f;
-    const float indentPercent = 0.05;
-    const float indentWidth = windowLength * indentPercent; // 40
-    const float lineWidth = 1.0f;
-    const float gridLength = windowLength - (2*indentWidth); // 720
-    const int gridSize = 8; // 40 to 760
-    const float squareLength = gridLength / gridSize; // 22.5
-    int tileMap[gridSize][gridSize] = {};
-
-    sf::RenderWindow window(sf::VideoMode(windowLength, windowLength), "Tilemap :)");
-    sf::RenderWindow palette(sf::VideoMode(400, 800), "Palette :I");
+    window.setPosition(sf::Vector2i(200, 0));
+    palette.setPosition(sf::Vector2i(window.getPosition().x+windowLength+indentWidth, windowLength*0.25));
 
     while (window.isOpen())
     {
@@ -68,14 +73,14 @@ int main()
         {
             if (event.type == sf::Event::Closed)
             {
-                for (int i=0;i<gridSize;i++)
-                {
-                    for (int j=0;j<gridSize;j++)
-                    {
-                        std::cout << tileMap[i][j] << "  ";
-                    }
-                    std::cout << "\n";
-                }
+                // for (int i=0;i<gridSize;i++)
+                // {
+                //     for (int j=0;j<gridSize;j++)
+                //     {
+                //         std::cout << tileMap[i][j] << "  ";
+                //     }
+                //     std::cout << "\n";
+                // }
                 window.close();
             }
         }
@@ -85,11 +90,11 @@ int main()
 
         //drawing stuff here
 
-        //draw transparent gray box over selected tile
-        drawIndicator(window, windowLength, indentWidth, gridLength, gridSize, squareLength, lineWidth);
-        
         //draw grid
-        drawGrid(window, lineWidth, indentWidth, gridLength, gridSize);
+        drawGrid();
+
+        //draw transparent gray box over selected tile
+        drawIndicator();
 
         window.display();
         palette.display();
